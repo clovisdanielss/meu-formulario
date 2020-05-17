b32ea22763ca105b89021b1620b075feeff3951f
clovisdanielss@gmail.com <clovisdanielss@gmail.com>
2020-05-17 15:55:22 -0300

    ajustando servidor

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ var app = express()
 var http = require('http')
 var path = require('path')
 var cors = require('cors')
-app.use(express.static(path.join(__dirname, 'build')))
+
 app.use(cors())
+
+app.use(express.static(path.join(__dirname, 'build')))
+
 app.PORT = process.env.PORT || 8080
 
 var server = http.createServer(app)
 
 server.listen(app.PORT, () => {
   console.log('listening in ', app.PORT)
+})
+
+app.get('*', (req, res, next) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 })
