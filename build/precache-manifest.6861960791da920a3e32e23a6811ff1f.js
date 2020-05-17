0acb4cb6329484d728f7be66f7613f0c80419cca
clovisdanielss@gmail.com <clovisdanielss@gmail.com>
2020-05-17 15:24:37 -0300

    build para heroku

---
+++ build/precache-manifest.6861960791da920a3e32e23a6811ff1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ca6ca9eb75cc0cda6f73c12c0c38f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87b0ae477e5ec737ccad",
+    "url": "/static/css/main.91e04a31.chunk.css"
+  },
+  {
+    "revision": "d00429219005784104ba",
+    "url": "/static/js/2.26c46658.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.26c46658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87b0ae477e5ec737ccad",
+    "url": "/static/js/main.aab4aebb.chunk.js"
+  },
+  {
+    "revision": "279021a56d19144b96ac",
+    "url": "/static/js/runtime-main.2cb9cb27.js"
+  }
+]);
