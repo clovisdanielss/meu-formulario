66cb5efcd21ce7e774f259b48ab77de5ee3124b5
clovisdanielss@gmail.com <clovisdanielss@gmail.com>
2020-05-17 15:31:52 -0300

    cors

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var express = require('express')
 var app = express()
 var http = require('http')
 var path = require('path')
+var cors = require('cors')
 app.use(express.static(path.join(__dirname, 'build')))
-
+app.use(cors())
 app.PORT = process.env.PORT || 8080
 
 var server = http.createServer(app)
